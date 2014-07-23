c5bd5dc99cc55f125acd42b3620f83b56f9dfa1e
Matteo Kloiber <info@matt3o12.de>
2014-07-23 14:59:20 +0200

    Adding some docs [no-ci]

--- markupfield2/fields.py
+++ markupfield2/fields.py
@@ -1,3 +1,4 @@
+from django.forms.fields import ChoiceField
 from django.utils.encoding import python_2_unicode_compatible
 from . import markup
 from django.utils.safestring import mark_safe
@@ -7,26 +8,63 @@ from django.db import models
 @python_2_unicode_compatible
 class Markup(object):
     def __init__(self, instance, raw_value, markup_field):
+        """
+        Markup is responsible for rendering the raw values and
+        providing the raw values if necessary.
+        The instance
+
+        :param instance: models.Model
+        :param raw_value: string
+        :param markup_field: MarkupField
+        """
         self.instance = instance
         self.markup_field = markup_field
         self._raw_value = raw_value
 
     @property
     def raw(self):
+        """
+        Return the raw, unrendered value.
+
+        :return: returns the raw, unrendered value
+        :rtype: string
+        """
         return self._raw_value
 
     @property
     def rendered(self):
+        """
+        Return the rendered value.
+
+        :return:
+        :rtype: SafeData
+        """
+
         return mark_safe(self.markup_field.markup_renderer(self.raw))
 
     def __str__(self):
+        """
+        Returns the raw data.
+        """
         return self.raw
 
 class MarkupFieldDescriptor(object):
     def __init__(self, field):
+        """
+        The descriptor for the markup field.
+
+        :param field: the field to use.
+        """
         self.field = field
 
     def __get__(self, instance, owner):
+        """
+
+        :return: None if there isn't a raw value.
+        :rtype: Markup or None
+        :raise: AttributeError if not accessed via an instance.
+        """
+        print type(instance)
         if instance is None:
             raise AttributeError('Can only be accessed via an instance.')
 
@@ -45,6 +83,13 @@ class MarkupFieldDescriptor(object):
 
 class MarkupField(models.TextField):
     def __init__(self, markup_type, *args, **kwargs):
+        """
+        Creates a new Markup Field.
+
+        :param markup_type: string markup_type to use.
+        :param args: Same as models.TextField
+        :param kwargs: Same as models.TextField
+        """
         super(MarkupField, self).__init__(*args, **kwargs)
 
         # TODO: Check whether markup_type is valid.
@@ -58,8 +103,18 @@ class MarkupField(models.TextField):
 
     @property
     def markup_renderer(self):
+        """
+        Return the renderer (html, plain, markdown) to use to render
+        the value.
+
+        :return: the renderer to use
+        """
         return markup.get_renderer(self.markup_type)
 
     def contribute_to_class(self, cls, name, *args, **kwargs):
+        """
+        Adds MarkupFieldDescriptor to the class.
+        Also calls its parent's method.
+        """
         super(MarkupField, self).contribute_to_class(cls, name, *args, **kwargs)
         setattr(cls, self.name, MarkupFieldDescriptor(self))
